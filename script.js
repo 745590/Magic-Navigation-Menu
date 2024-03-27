623c09ad031fb22fc4f7607ce73b3f617807ba9d
Abhishek Kumar <121302712+745590@users.noreply.github.com>
2024-03-28 01:03:08 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,237 @@
+var list1 = document.querySelectorAll(".list1");
+var list2 = document.querySelectorAll(".list2");
+var list3 = document.querySelectorAll(".list3");
+var list4 = document.querySelectorAll(".list4");
+var list5 = document.querySelectorAll(".list5");
+var container = document.querySelector(".navigation");
+var icons1 = document.querySelectorAll(".i1");
+var icons2 = document.querySelectorAll(".i2");
+var icons3 = document.querySelectorAll(".i3");
+var icons4 = document.querySelectorAll(".i4");
+var icons5 = document.querySelectorAll(".i5");
+
+list1.forEach(function(item) {
+    item.addEventListener("click", function() {
+        icons1.forEach(function(icon1) {
+            icon1.style.borderRadius = '50px'; 
+            icon1.style.border = '5px double #209cff';
+            icon1.style.padding = '10px';
+            icon1.style.backgroundColor = 'black';
+            icon1.style.color = '#209cff';
+            icon1.style.boxShadow = '5px 5px 5px rgba(0, 0 , 0 ,0.5 )';
+        });
+        icons4.forEach(function(icon4){
+            icon4.style.borderRadius = '50px'; 
+            icon4.style.border = 'none';
+            icon4.style.padding = '10px';
+            icon4.style.backgroundColor = 'white';
+            icon4.style.color = '#0000005f';
+            icon4.style.boxShadow = 'none';
+        });
+        icons2.forEach(function(icon2){
+            icon2.style.borderRadius = '50px'; 
+            icon2.style.border = 'none';
+            icon2.style.padding = '10px';
+            icon2.style.backgroundColor = 'white';
+            icon2.style.color = '#0000005f';
+            icon2.style.boxShadow = 'none';
+        });
+        icons3.forEach(function(icon3){
+            icon3.style.borderRadius = '50px'; 
+            icon3.style.border = 'none';
+            icon3.style.padding = '10px';
+            icon3.style.backgroundColor = 'white';
+            icon3.style.color = '#0000005f';
+            icon3.style.boxShadow = 'none';
+        });
+        icons5.forEach(function(icon5){
+            icon5.style.borderRadius = '50px'; 
+            icon5.style.border = 'none';
+            icon5.style.padding = '10px';
+            icon5.style.backgroundColor = 'white';
+            icon5.style.color = '#0000005f';
+            icon5.style.boxShadow = 'none';
+        });
+    });
+});
+
+
+list2.forEach(function(item) {
+    item.addEventListener("click", function() {
+        icons2.forEach(function(icon2) {
+            icon2.style.borderRadius = '50px'; 
+            icon2.style.border = '5px double #209cff';
+            icon2.style.padding = '10px';
+            icon2.style.backgroundColor = 'black';
+            icon2.style.color = '#209cff';
+            icon2.style.boxShadow = '5px 5px 5px rgba(0, 0 , 0 ,0.5 )';
+        });
+        icons1.forEach(function(icon1){
+            icon1.style.borderRadius = '50px'; 
+            icon1.style.border = 'none';
+            icon1.style.padding = '10px';
+            icon1.style.backgroundColor = 'white';
+            icon1.style.color = '#0000005f';
+            icon1.style.boxShadow = 'none';
+        });
+        icons4.forEach(function(icon4){
+            icon4.style.borderRadius = '50px'; 
+            icon4.style.border = 'none';
+            icon4.style.padding = '10px';
+            icon4.style.backgroundColor = 'white';
+            icon4.style.color = '#0000005f';
+            icon4.style.boxShadow = 'none';
+        });
+        icons3.forEach(function(icon3){
+            icon3.style.borderRadius = '50px'; 
+            icon3.style.border = 'none';
+            icon3.style.padding = '10px';
+            icon3.style.backgroundColor = 'white';
+            icon3.style.color = '#0000005f';
+            icon3.style.boxShadow = 'none';
+        });
+        icons5.forEach(function(icon5){
+            icon5.style.borderRadius = '50px'; 
+            icon5.style.border = 'none';
+            icon5.style.padding = '10px';
+            icon5.style.backgroundColor = 'white';
+            icon5.style.color = '#0000005f';
+            icon5.style.boxShadow = 'none';
+        });
+    });
+});
+
+list3.forEach(function(item) {
+    item.addEventListener("click", function() {
+        icons3.forEach(function(icon3) {
+            icon3.style.borderRadius = '50px'; 
+            icon3.style.border = '5px double #209cff';
+            icon3.style.padding = '10px';
+            icon3.style.backgroundColor = 'black';
+            icon3.style.color = '#209cff';
+            icon3.style.boxShadow = '5px 5px 5px rgba(0, 0 , 0 ,0.5 )';
+        });
+        icons1.forEach(function(icon1){
+            icon1.style.borderRadius = '50px'; 
+            icon1.style.border = 'none';
+            icon1.style.padding = '10px';
+            icon1.style.backgroundColor = 'white';
+            icon1.style.color = '#0000005f';
+            icon1.style.boxShadow = 'none';
+        });
+        icons2.forEach(function(icon2){
+            icon2.style.borderRadius = '50px'; 
+            icon2.style.border = 'none';
+            icon2.style.padding = '10px';
+            icon2.style.backgroundColor = 'white';
+            icon2.style.color = '#0000005f';
+            icon2.style.boxShadow = 'none';
+        });
+        icons4.forEach(function(icon4){
+            icon4.style.borderRadius = '50px'; 
+            icon4.style.border = 'none';
+            icon4.style.padding = '10px';
+            icon4.style.backgroundColor = 'white';
+            icon4.style.color = '#0000005f';
+            icon4.style.boxShadow = 'none';
+        });
+        icons5.forEach(function(icon5){
+            icon5.style.borderRadius = '50px'; 
+            icon5.style.border = 'none';
+            icon5.style.padding = '10px';
+            icon5.style.backgroundColor = 'white';
+            icon5.style.color = '#0000005f';
+            icon5.style.boxShadow = 'none';
+        });
+    });
+});
+
+list4.forEach(function(item) {
+    item.addEventListener("click", function() {
+        icons4.forEach(function(icon4) {
+            icon4.style.borderRadius = '50px'; 
+            icon4.style.border = '5px double #209cff';
+            icon4.style.padding = '10px';
+            icon4.style.backgroundColor = 'black';
+            icon4.style.color = '#209cff';
+            icon4.style.boxShadow = '5px 5px 5px rgba(0, 0 , 0 ,0.5 )'; 
+        });
+        icons1.forEach(function(icon1){
+            icon1.style.borderRadius = '50px'; 
+            icon1.style.border = 'none';
+            icon1.style.padding = '10px';
+            icon1.style.backgroundColor = 'white';
+            icon1.style.color = '#0000005f';
+            icon1.style.boxShadow = 'none';
+        });
+        icons2.forEach(function(icon2){
+            icon2.style.borderRadius = '50px'; 
+            icon2.style.border = 'none';
+            icon2.style.padding = '10px';
+            icon2.style.backgroundColor = 'white';
+            icon2.style.color = '#0000005f';
+            icon2.style.boxShadow = 'none';
+        });
+        icons3.forEach(function(icon3){
+            icon3.style.borderRadius = '50px'; 
+            icon3.style.border = 'none';
+            icon3.style.padding = '10px';
+            icon3.style.backgroundColor = 'white';
+            icon3.style.color = '#0000005f';
+            icon3.style.boxShadow = 'none';
+        });
+        icons5.forEach(function(icon5){
+            icon5.style.borderRadius = '50px'; 
+            icon5.style.border = 'none';
+            icon5.style.padding = '10px';
+            icon5.style.backgroundColor = 'white';
+            icon5.style.color = '#0000005f';
+            icon5.style.boxShadow = 'none';
+        });
+    });
+});
+
+list5.forEach(function(item) {
+    item.addEventListener("click", function() {
+        icons5.forEach(function(icon5) {
+            icon5.style.borderRadius = '50px'; 
+            icon5.style.border = '5px double #209cff';
+            icon5.style.padding = '10px';
+            icon5.style.backgroundColor = 'black';
+            icon5.style.color = '#209cff';
+            icon5.style.boxShadow = '5px 5px 5px rgba(0, 0 , 0 ,0.5 )';
+        });
+        icons1.forEach(function(icon1){
+            icon1.style.borderRadius = '50px'; 
+            icon1.style.border = 'none';
+            icon1.style.padding = '10px';
+            icon1.style.backgroundColor = 'white';
+            icon1.style.color = '#0000005f';
+            icon1.style.boxShadow = 'none';
+        });
+        icons2.forEach(function(icon2){
+            icon2.style.borderRadius = '50px'; 
+            icon2.style.border = 'none';
+            icon2.style.padding = '10px';
+            icon2.style.backgroundColor = 'white';
+            icon2.style.color = '#0000005f';
+            icon2.style.boxShadow = 'none';
+        });
+        icons3.forEach(function(icon3){
+            icon3.style.borderRadius = '50px'; 
+            icon3.style.border = 'none';
+            icon3.style.padding = '10px';
+            icon3.style.backgroundColor = 'white';
+            icon3.style.color = '#0000005f';
+            icon3.style.boxShadow = 'none';
+        });
+        icons4.forEach(function(icon4){
+            icon4.style.borderRadius = '50px'; 
+            icon4.style.border = 'none';
+            icon4.style.padding = '10px';
+            icon4.style.backgroundColor = 'white';
+            icon4.style.color = '#0000005f';
+            icon4.style.boxShadow = 'none';
+        });
+    });
+});
